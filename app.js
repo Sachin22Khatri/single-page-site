007f9e20ee2e2bc15000ff917a7e0fe120beda8d
Sachin22Khatri <sachinkhatri721@gmail.com>
2021-10-11 22:11:41 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,67 @@
+//image slider
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("work-life");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  captionText.innerHTML = dots[slideIndex - 1].alt;
+}
+
+
+
+// Video slider
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i,
+      x = document.getElementsByClassName("video-slide"),
+      y = document.getElementsByTagName("video");
+
+  if (n > x.length) {
+    slideIndex = 1
+  }
+
+  if (n < 1) {
+    slideIndex = x.length
+  }
+
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+    y[i].pause();
+  }
+
+  x[slideIndex-1].style.display = "block";
+
+}
